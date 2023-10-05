ed0fbc64dd17a28af3f714a703c46b7775897cf7
RomanOdessa75 <115027650+RomanOdessa75@users.noreply.github.com>
2023-10-05 12:18:44 +0300

    SR_test_task-meta-edit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,9 @@
 import { Inter } from "next/font/google";
 import Header from "@/components/Header/Header";
-// import Meta from "@/components/Meta/Meta";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-// export const metadata = {
-//   title: "CarpathianSecret",
-//   description: "CarpathianSecret Web-site",
-// };
 export const metadata = {
   title: "Carpathian's Secrets",
   description:
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,7 +5,7 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          {/* <meta property="og:title" content="Carpathian's Secrets" />
+          <meta property="og:title" content="Carpathian's Secrets" />
           <meta property="og:site_name" content="Carpathian's Secrets" />
           <meta
             property="og:description"
@@ -14,7 +14,7 @@ class MyDocument extends Document {
           <meta property="og:type" content="website" />
           <meta
             property="og:image"
-            content="https://softryzen-test.vercel.app/Carpatians-secrets.jpg"
+            content="https://softryzen-test.vercel.app/carpatians-secrets.png"
           />
           <meta property="og:image:type" content="image/jpeg" />
 
@@ -31,28 +31,11 @@ class MyDocument extends Document {
           <meta property="og:locala" content="en_US" />
           <meta
             name="twitter:image"
-            content="https://softryzen-test.vercel.app/Carpatians-secrets.jpg"
+            content="https://softryzen-test.vercel.app/carpatians-secrets.png"
           />
           <meta name="twitter:image:type" content="image/jpeg" />
           <meta name="twitter:image:width" content="1200px" />
-          <meta name="twitter:image:height" content="630px" /> */}
-		
-
-<!-- Open Graph / Facebook -->
-<meta property="og:type" content="website" />
-<meta property="og:url" content="https://metatags.io/" />
-<meta property="og:title" content="Carpathian's Secrets" />
-<meta property="og:description" content="Welcome to CARPATHIAN'S SECRETS! Our website is dedicated to all adventure seekers and nature lovers who want to explore the breathtaking beauty of mountains." />
-<meta property="og:image" content="https://metatags.io/images/meta-tags.png" />
-
-<!-- Twitter -->
-<meta property="twitter:card" content="summary_large_image" />
-<meta property="twitter:url" content="https://metatags.io/" />
-<meta property="twitter:title" content="Carpathian's Secrets" />
-<meta property="twitter:description" content="Welcome to CARPATHIAN'S SECRETS! Our website is dedicated to all adventure seekers and nature lovers who want to explore the breathtaking beauty of mountains." />
-<meta property="twitter:image" content="https://metatags.io/images/meta-tags.png" />
-
-<!-- Meta Tags Generated with https://metatags.io -->
+          <meta name="twitter:image:height" content="630px" />
         </Head>
         <body>
           <Main />
